c67eee10eff97a14c2c8f48e04982bb631d08dda
Laércio Vitorino <laerciovitorino@gmail.com>
2018-04-17 11:49:25 -0300

    Add playground folder and some tests with JSON

---
+++ playground/json.js
@@ -0,0 +1,25 @@
+// let obj = {
+//   name: 'Laercio'
+// };
+// let stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// let personString = '{"name": "Laercio", "age": 31}';
+// let person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+let originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
